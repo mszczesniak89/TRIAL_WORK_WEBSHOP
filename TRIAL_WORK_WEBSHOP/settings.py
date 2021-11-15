7795de857582e19f18c439f1510308b6cf525a5a
Mateusz Szczesniak <mszczesniak@live.com>
2021-11-15 12:04:50 +0100

    15NOV-general-tweaks

--- TRIAL_WORK_WEBSHOP/settings.py
+++ TRIAL_WORK_WEBSHOP/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,4 @@ RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+django_heroku.settings(locals())

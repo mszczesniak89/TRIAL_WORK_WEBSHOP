7f63f317b21169519c33ae305ad945bcda356db5
Mateusz Szczesniak <mszczesniak@live.com>
2021-11-15 13:12:43 +0100

    15NOV-general-tweaks

--- TRIAL_WORK_WEBSHOP/settings.py
+++ TRIAL_WORK_WEBSHOP/settings.py
@@ -155,8 +155,5 @@ RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 django_heroku.settings(locals())
 
